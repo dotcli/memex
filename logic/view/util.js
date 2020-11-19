655fef2216f01c8f177b1cfbc8a460d753b4206b
lcb931023 <lcb931023@gmail.com>
2020-11-19 15:39:17 +0800

    add forum type

--- logic/view/util.js
+++ logic/view/util.js
@@ -28,6 +28,7 @@ function Util()
         case 'encyclopedia': icon = 'fas fa-globe'; break;
         case 'interactive': icon = 'fas fa-mouse-pointer'; break;
         case 'drink': icon = 'fas fa-glass-martini-alt'; break;
+        case 'forum': icon = 'fas fa-users'; break;
         case 'term': icon = 'fas fa-ribbon'; break;
         case 'note': icon = 'fas fa-sticky-note'; break;
         case 'date': icon = 'fas fa-clock'; break;
